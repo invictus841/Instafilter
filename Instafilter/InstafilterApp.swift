c8b474cebe0db36cfd2404b5689a965b5e0e844e
NT <usedforfunny@gmail.com>
2022-09-01 15:35:38 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Alexandre Talatinian on 01/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
